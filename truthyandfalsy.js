f2bee0d91ccc6ada506d8d21c2ec7fb0a423bb26
Rakshith272003 <rakshithmdpura@gmail.com>
2024-11-27 21:36:38 +0530

    daily updates

---
+++ truthyandfalsy.js
@@ -0,0 +1,39 @@
+/*TRUTHY AND FALSY IN JS*/
+
+/*Falsy values in JS are : false,0,-0,"",null,undefined,NaN,etc..*/
+
+if(true){
+    console.log("it is TRUTHY")//dark in color
+}
+else{
+    console.log("it is FALSY")
+}
+
+//output is "it is TRUTHY"//
+
+if(false){
+    console.log("it is TRUTHY")//light in color
+}
+else{
+    console.log("it is FALSY")
+}
+
+//output is "it is falsy"//
+
+if(1){
+    console.log("it is TRUTHY")
+}
+else{
+    console.log("it is FALSY")
+}
+
+//output is "it is TRUTHY"//
+
+if(0){
+    console.log("it is TRUTHY")
+}
+else{
+    console.log("it is FALSY")
+}
+
+//output is "it is Falsy"//
